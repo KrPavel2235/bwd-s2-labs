f16f27d7bb61a007797e70a18fa9398adba13c5f
PAVEL <dcsuper1337@gmail.com>
2025-03-17 01:08:12 +0400

    refactor: LAB1 изменена структура кода

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,10 +1,13 @@
-export function createUser(name, email) {
+import User from '../models/User.js'; // Импортируем модель User
+import { CustomError } from '../config/error.js';
+export async function createUserCheck(email) {
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-        return new Error("Пользователь с таким email уже существует"); //todo CustomErr(400, "...")
+        throw new CustomError('Пользователь с таким email уже существует');
     }
+}
 
+export async function createUsers(name, email) {
     const user = await User.create({ name, email });
-
     return user;
 }
